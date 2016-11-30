2e06dc73ac3df7aef57b3622e92fa1a3a2982a49
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2016-11-30 17:58:43 +0100

    added more unittests

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.org/kiliakis/py-unittests.svg?branch=master)](https://travis-ci.org/kiliakis/py-unittests)
+[![Coverage Status](https://coveralls.io/repos/github/kiliakis/py-unittests/badge.svg?branch=master)](https://coveralls.io/github/kiliakis/py-unittests?branch=master)
 ## py-unittests
 Simple example repo for python unittesting
 
--- unittests/test_MyClass1.py
+++ unittests/test_MyClass1.py
@@ -16,5 +16,21 @@ class TestMyClass1(unittest.TestCase):
         self.assertEqual(obj.b, 4.5)
         self.assertEqual(obj.c, 8)
 
+    def test_product1(self):
+        obj = MyClass1(1, 2, 3)
+        self.assertEqual(obj.getProduct(), 6)
+
+    def test_min1(self):
+        obj = MyClass1(2, 2, 2)
+        self.assertEqual(obj.getMin(), 2)
+
+    def test_min2(self):
+        obj = MyClass1(-10, 2, 3)
+        self.assertEqual(obj.getMin(), -10)
+
+    def test_max(self):
+        obj = MyClass1(1, 2, 3)
+        self.assertEqual(obj.getMax(), 3)
+
 if __name__ == '__main__':
     unittest.main()
